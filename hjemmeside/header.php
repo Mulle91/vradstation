a37dab54bf404785322b7146feb9cf69176f49c2
Nicklas <NPMa.skp@edu.mercantec.dk>
2018-01-08 10:42:41 +0100

    opbygning af topnav

--- hjemmeside/header.php
+++ hjemmeside/header.php
@@ -1,11 +1,20 @@
-<?php
-?>
-
 <html>
 
 <head>
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>hej</title>
 </head>
 
 <body>
+
+<div class="topnav">
+	<img class="logo" src="test.png">
+	<ul>
+		<li class="konceptet"><a href=""> Konceptet </a></li>
+		<li class="mad"><a href=""> Mad & drikke </a></li>
+		<li class="book"><a href=""> Book et bord </a></li>
+		<li class="gave"><a href=""> Gavekort </a></li>
+		<li class="info"><a href=""> Info & kontakt </a></li>
+	</ul>
+</div>
