91890d456309b59cef816fe2dd9090cb6a03a3a8
Nicklas <NPMa.skp@edu.mercantec.dk>
2018-01-09 16:58:11 +0100

    rettelser

--- hjemmeside/index.php
+++ hjemmeside/index.php
@@ -2,12 +2,10 @@
 include "header.php";
 ?>
 
-<h1 class="head">Velkommen til Vradsstation.dk</h1>
-
 <div class="topbilled">
-<img id="v" src="css/vbilled.jfif" style="left: 3%;">
+<img id="v" src="css/vbilled.jfif">
 
-<img id="r" src="css/rbilled.jpg" style="left: 50.5%;">
+<img id="r" src="css/rbilled.jpg">
 </div>
 
 <?php
