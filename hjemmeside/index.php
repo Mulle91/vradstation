32649d855982c1ca62edc694c7772ee805c8dd3f
Nicklas <NPMa.skp@edu.mercantec.dk>
2018-01-09 13:43:55 +0100

    udvikling på index. lille rettelse til topnavn.css

--- hjemmeside/index.php
+++ hjemmeside/index.php
@@ -4,6 +4,12 @@ include "header.php";
 
 <h1 class="head">Velkommen til Vradsstation.dk</h1>
 
+<div class="topbilled">
+<img id="v" src="css/vbilled.jfif" style="left: 3%;">
+
+<img id="r" src="css/rbilled.jpg" style="left: 50.5%;">
+</div>
+
 <?php
 include "footer.php";
 ?>
