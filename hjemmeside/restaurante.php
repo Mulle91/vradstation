af35366d704984116648ad177e39011688b49d1f
Nicklas <NPMa.skp@edu.mercantec.dk>
2018-02-06 16:51:44 +0100

    gået så småt i gang med restaurante.php

--- hjemmeside/restaurante.php
+++ hjemmeside/restaurante.php
@@ -2,6 +2,14 @@
 include "header.php";
 ?>
 
+<div class="topbilled">
+	<img src="css/restimg.jpg">
+</div>
+
+<div class="brødtekst">
+	<h1>Lidt mere info om restauranten Vradsstation</h1>
+</div>
+
 <?php 
 include "footer.php";
 ?>
