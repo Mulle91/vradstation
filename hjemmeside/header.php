b36f6e6f2c63e27f8bd738135465e62502934203
Nicklas <NPMa.skp@edu.mercantec.dk>
2018-02-27 14:14:38 +0100

    no message

--- hjemmeside/header.php
+++ hjemmeside/header.php
@@ -15,46 +15,46 @@ include "script.php";
 <body>
 
 <div class="topback">
-	<a href="http:\\192.168.114.48"><img class="logo" src="css/billeder/logo.png" style="width: 300px; float: right;"></a>
+	<a href=""><img class="logo" src="css/billeder/logo.png" style="width: 300px; float: right;"></a>
 	
 	<div class="topnav">
 
 		<nav>
 			<ul>
 				<li>
-					<a href="http:\\192.168.114.48\#Menu"> Menu </a>
+					<a href="/#Menu"> Menu </a>
 				</li>
 
 				<li>
-					<a href="http:\\192.168.114.48\#Vin"> Vin </a>
+					<a href="/#Vin"> Vin </a>
 				</li>
 
 				<li>
-					<a href="http:\\192.168.114.48\#Selskab"> Selskaber </a>
+					<a href="/#Selskab"> Selskaber </a>
 				</li>
 
 				<li>
-					<a href="http:\\192.168.114.48\#Arrangementer"> Arrangementer </a>
+					<a href="/#Arrangementer"> Arrangementer </a>
 				</li>
 
 				<li>
-					<a href="http:\\192.168.114.48\#Restauranten"> Restauranten </a>
+					<a href="/#Restauranten"> Restauranten </a>
 				</li>
 				
 				<li>
-					<a href="http:\\192.168.114.48\#Kontakt"> Info og kontakt </a>
+					<a href="/#Kontakt"> Info og kontakt </a>
 				</li>
 			</ul>
 
 			<select>
 				<option selected="selected" value>gå til</option>
 
-				<option value="http:\\192.168.114.48\#Menu">Menu</option>
-				<option value="http:\\192.168.114.48\#Vin">Vin</option>
-				<option value="http:\\192.168.114.48\#Selskab">Selskab</option>
-				<option value="http:\\192.168.114.48\#Arrangementer">Arrangementer</option>
-				<option value="http:\\192.168.114.48\#Restauranten">Restauranten</option>
-				<option value="http:\\192.168.114.48\#Kontakt">Kontakt os</option>
+				<option value="/#Menu">Menu</option>
+				<option value="/#Vin">Vin</option>
+				<option value="/#Selskab">Selskab</option>
+				<option value="/#Arrangementer">Arrangementer</option>
+				<option value="/#Restauranten">Restauranten</option>
+				<option value="/#Kontakt">Kontakt os</option>
 			</select>
 		</nav>
 	</div>
