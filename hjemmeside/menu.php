9759682383d30fcbd679023c948b3d9d81bf3c27
Nicklas <NPMa.skp@edu.mercantec.dk>
2018-03-06 16:50:39 +0100

    no message

--- hjemmeside/menu.php
+++ hjemmeside/menu.php
@@ -6,62 +6,68 @@ include "header.php";
 	<div id="Menu">
 
 		<h1 id="kort" style="text-align: center;">Menukort</h1>
-		<table>
-			<tr>
-				<td class="menutekst">
-					<h3>Frokost & aftenmenu</h3>
-					<p>Restaurant Vrads Stations menukort er a la carte retter, <br>
-						som serveres både til frokost og til aften mellem <br>
-						kl. 12-21.00 <br>
-						Vi sætter en stor ære i at tilberede alle retter fra bunden og naturligvis af sæsonens bedste råvarer. <br>
-						Med inspiration fra både det franske og moderne danske køkken bestræber vi os på, <br>at give jer en smags fuld oplevelse i tæt selskab med naturen.
+		<div class="test">
+			<table>
+				<tr>
+					<td class="menutekst">
+						<h3>Frokost & aftenmenu</h3>
+						<p>Restaurant Vrads Stations menukort er a la carte retter, <br>
+							som serveres både til frokost og til aften mellem <br>
+							kl. 12-21.00 <br>
+							Vi sætter en stor ære i at tilberede alle retter fra bunden og naturligvis af sæsonens bedste råvarer. <br>
+							Med inspiration fra både det franske og moderne danske køkken bestræber vi os på, <br>at give jer en smags fuld oplevelse i tæt selskab med naturen.
 
-					</p>
-				</td>
-				<td>
-					<img src="css/billeder/restaurantImg.jpg">
-				</td>
-			</tr>
-		</table>
+						</p>
+					</td>
+					<td>
+						<img src="css/billeder/restaurantImg.jpg">
+					</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 
 	<div id="Menu">
-		<table>
-			<tr>
-				<td>
-					<img src="css/billeder/restaurantImg.jpg">
-				</td>
-				<td class="menutekst" style="position: relative; right: 5%;">
-					<h3>Onsdags menu</h3>
-					<p>Vi tilbyder hver onsdag en 2 retters menu efter stationens eget valg. <br>
-						Her vil vi servere klassiske retter med et twist, som skifter hver uge. <br>
-						Husk at bestille bord, så vi kan sikre, <br>
-						at der er plads til jer i stationens stemningsfulde lokaler. <br> <br> 
+		<div class="test">
+			<table>
+				<tr>
+					<td>
+						<img src="css/billeder/restaurantImg.jpg">
+					</td>
+					<td class="menutekst" style="position: relative; right: 5%;">
+						<h3>Onsdags menu</h3>
+						<p>Vi tilbyder hver onsdag en 2 retters menu efter stationens eget valg. <br>
+							Her vil vi servere klassiske retter med et twist, som skifter hver uge. <br>
+							Husk at bestille bord, så vi kan sikre, <br>
+							at der er plads til jer i stationens stemningsfulde lokaler. <br> <br> 
 
-						Prisen for 2 retter inkl. 2 glas vin, øl eller sodavand er 245,- 
-					</p>
-				</td>
-			</tr>
-		</table>
+							Prisen for 2 retter inkl. 2 glas vin, øl eller sodavand er 245,- 
+						</p>
+					</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 
 	<div id="Menu">
-		<table>
-			<tr>
-				<td class="menutekst">
-					<h3>Kaffe & kage</h3>
-					<p>Vi står klar tirsdag-søndag kl. 12-17 med stationens lagkage, <br>
-						hjemmebag og 
-						friskbrygget kaffe. <br>
-						Der vil naturligvis også være mulighed for at nyde vores hjemmelavede varme chokolade med flødeskum, <br>
-						te eller en iskold specialøl fra ét af Danmarks mindste mikro bryggerier – Ikast mikrobryg.
-					</p>
-				</td>
-				<td>
-					<img src="css/billeder/restaurantImg.jpg">
-				</td>
-			</tr>
-		</table>
+		<div class="test">
+			<table>
+				<tr>
+					<td class="menutekst">
+						<h3>Kaffe & kage</h3>
+						<p>Vi står klar tirsdag-søndag kl. 12-17 med stationens lagkage, <br>
+							hjemmebag og 
+							friskbrygget kaffe. <br>
+							Der vil naturligvis også være mulighed for at nyde vores hjemmelavede varme chokolade med flødeskum, <br>
+							te eller en iskold specialøl fra ét af Danmarks mindste mikro bryggerier – Ikast mikrobryg.
+						</p>
+					</td>
+					<td>
+						<img src="css/billeder/restaurantImg.jpg">
+					</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 </div>
 
