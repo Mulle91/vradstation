8dfa5f32913e2aec9e47ddd104cf471ab1f08e30
Nicklas <sejenick@live.dk>
2018-03-15 12:32:22 +0100

    no message

--- hjemmeside/footer.php
+++ hjemmeside/footer.php
@@ -10,19 +10,19 @@
 				</td>
 
 				<td>
-					<h1>Resevere bord</h1>
-					<p>Tlf.: 75 75 62 20</p>
-
-					<h3>Spørgsmål</h3>
-					<p>Skriv til vores mail: info@vradsstation.dk</p>
-				</td>
-
-				<td>
 					<h1>Åbningstider:</h1>
 					<p>Mandag: Lukket <br>
 						Tirsdag: 18-21 <br>
 						Lørdag: 12-21 <br>
 						Søndag: 12-17</p>
+				</td>
+
+				<td>
+					<h1>Resevere bord</h1>
+					<p>Tlf.: 75 75 62 20</p>
+
+					<h3>Spørgsmål</h3>
+					<p>Skriv til vores mail: info@vradsstation.dk</p>
 				</td>
 			</tr>
 		</table>
